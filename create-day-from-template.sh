c1b2550e9448a0406fb8110e29dd4ff2b39908c8
Elmar Weber <elmar.weber@cupenya.com>
2023-12-31 11:30:06 +0000

    Update file extensions and create input files for day template

--- create-day-from-template.sh
+++ create-day-from-template.sh
@@ -10,7 +10,7 @@ if [ ${#1} -eq 1 ]
   then
     day="0$1"
 fi
-cp src/day_template.ts src/day${day}.js
-cp src/day_template.ts src/day${day}b.js
+cp src/day_template.ts src/day${day}.ts
+cp src/day_template.ts src/day${day}b.ts
 touch inputs/day${day}.txt
 touch inputs/day${day}_test.txt
